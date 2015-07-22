c4e1e0485291bba6c69637444e835920fb6c44bb
Katy Justiss <katy.justiss@gmail.com>
2015-07-22 11:41:28 -0500

    added cheers functionality and tests

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var should = require("chai").should();
+var cp = require('child_process');
+
+//Tests for the cheers function
+describe('Cheers', function() {
+  it('should create a cheer from name', function(done) {
+    cp.execFile('./app.js', ['Katy'], function(err, stdout) {
+      stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\n");
+      done();
+    })
+  })
+  it('should create cheer that can handle spaces', function(done) {
+    cp.execFile('./app.js', ['Katy', 'Ann'], function(err, stdout) {
+      stdout.should.equal("Give me a K!\nGive me an A!\nGive me a T!\nGive me a Y!\nGive me an A!\nGive me an N!\nGive me an N!\n");
+      done();
+    })
+  })
+});
